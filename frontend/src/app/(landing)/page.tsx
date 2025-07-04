9e9f44ce583be5bd989a5ba5aa6e39fdb4069981
Andreas Yuji Fujiki <andreaspinheiroprincipal@gmail.com>
2025-07-04 00:33:40 -0300

    feat(page.tsx): Added app header and hero section components into slash route page.

---
+++ frontend/src/app/(landing)/page.tsx
@@ -0,0 +1,11 @@
+import AppHeader from "@/components/AppHeader";
+import HeroSection from "@/components/HeroSection";
+
+export default function Home(){
+    return(
+        <div>
+            <AppHeader />
+            <HeroSection />
+        </div>
+    );
+};
